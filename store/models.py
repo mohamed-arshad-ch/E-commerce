61b5febbbe4f1fd1693ea22693c731a9ccaf3e01
Mohamed Arshad <mohamedarshadcholasseri5050@gmail.com>
2020-09-02 18:24:14 +0530

    users use complete

--- store/models.py
+++ store/models.py
@@ -22,6 +22,15 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
+
+
 
 class Order(models.Model):
     Customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,null=True,blank=True)
@@ -33,11 +42,26 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+   
+
 class OrderItem(models.Model):
      Product = models.ForeignKey(Product,on_delete=models.SET_NULL,null=True,blank=True)
      order = models.ForeignKey(Order,on_delete=models.SET_NULL,null=True,blank=True)
      quantity = models.IntegerField(default=0,null=True,blank=True)
      date_added = models.DateTimeField(auto_now_add=True)
+
+     @property
+     def get_total(self):
+         total= self.Product.price * self.quantity
+         return total
+
+    
 
 
 
--- store/views.py
+++ store/views.py
@@ -8,7 +8,14 @@ def store(request):
     return render(request,'index.html',context)
 
 def cart(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(Customer=customer,complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0}
+    context = {'items':items, 'order':order}
     return render(request,'cart.html',context)
 
 def checkout(request):
